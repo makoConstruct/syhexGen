ffc3f95681e834e358bfbfd23d1cc56157251d3e
mako <marcus-yass@ihug.co.nz>
2014-06-27 13:38:31 +1200

    removed WellRNG dependency

--- hexfaces.js
+++ hexfaces.js
@@ -1,15 +1,31 @@
 /* author: mako yass */
 
-/* note: depends Wellrng.js   https://github.com/gmalysa/well-rng */
-function newWell(seed){
-	var star = new Array(32);
-	for(var i=0; i<32; ++i){
-		var cell = ((seed&(i*5)) ^ (seed>>i)); //this is all kind of arbitrary, but you'd be surprised how many methods didn't successfully initialize the rng.
-		star[i] = cell & cell;} //[converts to int32]
-	star[0] = seed;
-	return new WELL(star);
+imul = Math.imul || function imul(a, b) {
+  var ah  = (a >>> 16) & 0xffff;
+  var al = a & 0xffff;
+  var bh  = (b >>> 16) & 0xffff;
+  var bl = b & 0xffff;
+  // the shift by 0 fixes the sign on the high part
+  // the final |0 converts the unsigned value into a signed value
+  return ((al * bl) + (((ah * bl + al * bh) << 16) >>> 0)|0);
 }
-/* parenthetical: (We don't need a secure hash, we just need a standard hash that can be repeated in a C environment. If someone could whip up a shoddier more efficient psuedohash that works the same as a minimal add, multiply and modulo psuedohash in C, that'd be appreciated.) */
+
+function perfectLCG(v){
+	return (imul(v,1121) + 2451) % 3136;
+}
+function faceSpecFromNumber(n){ //returns a facespec
+	var middles = n & ((1 << 6) - 1);
+	n = n >> 6;
+	var topline = (n % 7) + 1;
+	n = ((n|0) / 7)|0;
+	var bottomline = (n) + 1;
+	function bit(m,n){return (m>>n)&1}
+	return [
+		bit(topline,0), bit(middles,0), bit(bottomline,0), 
+		bit(topline,1), bit(middles,1), bit(middles,2), bit(bottomline,1), 
+		bit(topline,2), bit(middles,3), bit(middles,4), bit(middles,5), bit(bottomline,2)
+	];
+}
 
 
 
@@ -31,9 +47,7 @@ function newWell(seed){
 
 var hexfaces_root3over4 = Math.sqrt(3/4);
 var hexfaces_outerOverInner = Math.sqrt(5/4);
-var hexfaces_conf = {
-	cellsAcross : 5, cellsDown : 5 //you can change/parametize these if you like. Note, w should be an odd number, or else weirdness.
-}
+var cellsAcross = 5; var cellsDown = 5;
 
 function drawHexFace(con, spec, iconFillStyle, circleFillStyle, innerProportion){ //draws one over a circle, where radius of the inner icon = (radius of circle)*innerProportion. innerProportion is optional and defaults to 0.8.
 	var scaleFactor = innerProportion || 0.75;
@@ -50,7 +64,7 @@ function drawHexFace(con, spec, iconFillStyle, circleFillStyle, innerProportion)
 	con.arc(wi/2, hi/2, ra, 0, Math.PI*2);
 	con.fill();
 	con.fillStyle = iconFillStyle;
-	if(typeof(spec) == "array"){
+	if(spec instanceof Array){
 		pathHexFaceFromSpec(con, spec, sx, sy, swi, shi);
 	}else{
 		pathHexFace(con, spec, sx, sy, swi, shi);
@@ -63,50 +77,7 @@ function pathHexFace(con, seed, xin, yin, wi, hi){ //paths a centered maximized 
 	var hi = hi || con.canvas.height;
 	var xin = xin || 0;
 	var yin = yin || 0;
-	var widthOverHeight = ((hexfaces_conf.cellsAcross-1)*hexfaces_root3over4 + 1/hexfaces_outerOverInner)/(hexfaces_conf.cellsDown-1 + 1/hexfaces_outerOverInner);
-	var span = ((wi < hi*widthOverHeight)?wi:hi*widthOverHeight);
-	var hexradius =
-		span/(2*(hexfaces_conf.cellsAcross-1)*hexfaces_root3over4 + 2); // ': (hexfaces_conf.cellsAcross-1)*hexradius*2*hexfaces_root3over4 + hexradius*2 == span
-	var katy = newWell(seed ^ 666);
-	var x = xin + wi/2 - Math.floor(hexfaces_conf.cellsAcross/2)*2*hexradius*hexfaces_root3over4;
-	var y = yin + hi/2 - (hexfaces_conf.cellsDown-1)*hexradius /*which is the top cell in the glyph*/ + hexradius*Math.floor(hexfaces_conf.cellsAcross/2);
-	
-	//make syhexSpec from seed
-	
-	// var presents = katy.randBits(32);
-	// var bitsTaken = 0;
-	var takeRBit = function(){
-		return katy.randInt(0,1);
-		// if(bitsTaken == 31){
-		// 	bitsTaken = 0;
-		// 	presents = katy.randBits(32);
-		// }
-		// return 1 & (presents >> (bitsTaken++));
-	};
-	var off = Math.floor(hexfaces_conf.cellsAcross/2);
-	spec = new Array((hexfaces_conf.cellsDown-off)*(off+1) + (off*(off+1))/2);
-	var counter=0;
-	do{
-		if(++counter > 9000)
-			throw "something's wrong. katy isn't generating any valid candidates.";
-		for(var i=0; i<spec.length; ++i)
-			spec[i] = takeRBit();
-		var topHasSomething = false;
-		var bottomHasSomething = false;
-		for(var i=0, col=hexfaces_conf.cellsDown-off; i<spec.length; i += (col++)){
-			if(spec[i] == 1){
-				topHasSomething = true;
-				break;
-			}
-		}
-		for(var i=hexfaces_conf.cellsDown-off-1, col=hexfaces_conf.cellsDown-off; i<spec.length; i += (++col)){
-			if(spec[i] == 1){
-				bottomHasSomething = true;
-				break;
-			}
-		}
-	}while(!topHasSomething || !bottomHasSomething);
-	pathHexFaceFromSpec(con, spec, xin, yin, wi, hi);
+	pathHexFaceFromSpec(con, faceSpecFromNumber(perfectLCG(seed^666)), xin, yin, wi, hi); //Don't ask me why I'm doing the xor, it just seems to yeild better icons.
 }
 
 function pathHexFaceFromSpec(con, spec, xin, yin, wi, hi){ //allows to specify the icon, a spec is a column major array of the cells of the left side of the hex, 1 for filled, 0 for empty
@@ -115,20 +86,20 @@ function pathHexFaceFromSpec(con, spec, xin, yin, wi, hi){ //allows to specify t
 	var hi = hi || con.canvas.height;
 	var xin = xin || 0;
 	var yin = yin || 0;
-	var sheet = new Array(hexfaces_conf.cellsAcross*hexfaces_conf.cellsDown);
-	var arms = Math.ceil(hexfaces_conf.cellsAcross/2);
+	var sheet = new Array(cellsAcross*cellsDown);
+	var arms = Math.ceil(cellsAcross/2);
 	var lXStart = 0;
 	var lYStart = 0;
-	var columnLength = hexfaces_conf.cellsDown - (arms-1);
-	var rXStart = hexfaces_conf.cellsAcross-1;
+	var columnLength = cellsDown - (arms-1);
+	var rXStart = cellsAcross-1;
 	var rYStart = arms-1;
 	var specc = 0;
 	while(arms > 0){
 		for(var d=0; d<columnLength; ++d){
 			if(spec[specc++] == 1){
-				sheet[lXStart + (lYStart+d)*hexfaces_conf.cellsAcross] = 1;
+				sheet[lXStart + (lYStart+d)*cellsAcross] = 1;
 				if(lXStart != rXStart)
-					sheet[rXStart + (rYStart+d)*hexfaces_conf.cellsAcross] = 1;
+					sheet[rXStart + (rYStart+d)*cellsAcross] = 1;
 			}
 		}
 		--arms;
@@ -141,23 +112,23 @@ function pathHexFaceFromSpec(con, spec, xin, yin, wi, hi){ //allows to specify t
 	
 	//draw the sheet
 	con.beginPath();
-	var widthOverHeight = ((hexfaces_conf.cellsAcross-1)*hexfaces_root3over4 + 1/hexfaces_outerOverInner)/(hexfaces_conf.cellsDown-1 + 1/hexfaces_outerOverInner);
+	var widthOverHeight = ((cellsAcross-1)*hexfaces_root3over4 + 1/hexfaces_outerOverInner)/(cellsDown-1 + 1/hexfaces_outerOverInner);
 	var span = ((wi < hi*widthOverHeight)?wi:hi*widthOverHeight);
 	var hexradius =
-		span/(2*(hexfaces_conf.cellsAcross-1)*hexfaces_root3over4 + 2); // ': (hexfaces_conf.cellsAcross-1)*hexradius*2*hexfaces_root3over4 + hexradius*2 == span
-	var x = xin + wi/2 - Math.floor(hexfaces_conf.cellsAcross/2)*2*hexradius*hexfaces_root3over4;
-	var y = yin + hi/2 - (hexfaces_conf.cellsDown-1)*hexradius /*which is the top cell in the glyph*/ + hexradius*Math.floor(hexfaces_conf.cellsAcross/2);
+		span/(2*(cellsAcross-1)*hexfaces_root3over4 + 2); // ': (cellsAcross-1)*hexradius*2*hexfaces_root3over4 + hexradius*2 == span
+	var x = xin + wi/2 - Math.floor(cellsAcross/2)*2*hexradius*hexfaces_root3over4;
+	var y = yin + hi/2 - (cellsDown-1)*hexradius /*which is the top cell in the glyph*/ + hexradius*Math.floor(cellsAcross/2);
 	var circ = function(x,y){
 		con.arc(x,y,hexradius/hexfaces_outerOverInner,0,Math.PI*2,true);
 		con.closePath();
 	}
-	// var hex = function(x,y){ //like circ, only has sharp corners.
+	// var circ = function(x,y){ //like a circle, but with 6 sharp corners.
 	// 	con.moveTo(x, y + hexradius);
-	// 	con.lineTo(x - hexradius*hexfaces_root3over4, y + hexradius/2);
-	// 	con.lineTo(x - hexradius*hexfaces_root3over4, y - hexradius/2);
-	// 	con.lineTo(x, y - hexradius);
-	// 	con.lineTo(x + hexradius*hexfaces_root3over4, y - hexradius/2);
 	// 	con.lineTo(x + hexradius*hexfaces_root3over4, y + hexradius/2);
+	// 	con.lineTo(x + hexradius*hexfaces_root3over4, y - hexradius/2);
+	// 	con.lineTo(x, y - hexradius);
+	// 	con.lineTo(x - hexradius*hexfaces_root3over4, y - hexradius/2);
+	// 	con.lineTo(x - hexradius*hexfaces_root3over4, y + hexradius/2);
 	// 	con.closePath();
 	// }
 	var ulf = function(ox,oy){
@@ -188,11 +159,11 @@ function pathHexFaceFromSpec(con, spec, xin, yin, wi, hi){ //allows to specify t
 	};
 	var hexAt = function(ix,iy){
 		if( /*excuse the unterse style, my chrome has a bug*/
-			ix < 0 || ix >= hexfaces_conf.cellsAcross ||
-			iy < 0 || iy >= hexfaces_conf.cellsDown)
+			ix < 0 || ix >= cellsAcross ||
+			iy < 0 || iy >= cellsDown)
 				return false;
 			else
-				return sheet[ix + iy*hexfaces_conf.cellsAcross] == 1;
+				return sheet[ix + iy*cellsAcross] == 1;
 	}
 	var toCanv = function(ix,iy){
 		return [
@@ -200,8 +171,8 @@ function pathHexFaceFromSpec(con, spec, xin, yin, wi, hi){ //allows to specify t
 			y + iy*hexradius*2 - ix*hexradius
 		];
 	}
-	for(var xi=0; xi<hexfaces_conf.cellsAcross; ++xi){
-		for(var yi=0; yi<hexfaces_conf.cellsDown; ++yi){
+	for(var xi=0; xi<cellsAcross; ++xi){
+		for(var yi=0; yi<cellsDown; ++yi){
 			//each cell here draws a line to the cells on its left and top iff those cells are lit.
 			if(hexAt(xi,yi)){
 				var l = toCanv(xi,yi);
--- readme.md
+++ readme.md
@@ -1,9 +1,7 @@
 *interactive example*: [syhexGen](http://makopool.com/syhexGen.html)
 
-*note*: depends Wellrng.js, get as rng.js from https://github.com/gmalysa/well-rng
-
-(*parenthetically*: We don't need a secure hash, we just need a standard hash that can be repeated in a C environment. If someone could whip up a shoddier more efficient psuedohash that works the same in javascript as a minimal add, multiply and modulo psuedohash in C, that'd be appreciated.)
-
+*Parenthetical News*: There is no longer a dependency on WellRNG.js. We're now using a linear congruential generator for hashing seeds and producing facespecs from them (parameters: m=3136, c=1121, a=2451). This ensures that if you use your ids as the seed for each face, you wont have a single repetition prior to id number 3136. The period is as long as it possibly can be.
+LCG functions are also astonishingly efficient. Not that that matters at all here.
 
 	//usage examples:
 	canvas = document.createElement('canvas');
@@ -15,7 +13,7 @@
 	
 or like,
 
-	con.fillStyle = '#ffffff'
+	con.fillStyle = '#rgb(120,120,120)'
 	pathHexFace(con, seed)
 	con.fill();
 
